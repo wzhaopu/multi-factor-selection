8bbe55a5b35a6c1f92e8f77727439856075b24bd
huuuJL <534218580hu@gmail.com>
2019-06-28 12:32:08 +0800

    update

--- multifactor.py
+++ multifactor.py
@@ -1,60 +1,99 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import pandas as pd
 import time
 
-import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-tf.logging.set_verbosity(tf.logging.ERROR) 
-# Set to INFO for tracking training, default is WARN 
-
-df = pd.read_csv("new_stock.csv")
-temp_list = list(df.columns)
-
-
-for i in range(0,5):
-    temp_list.pop(0)
-
-print(temp_list)
+tf.logging.set_verbosity(tf.logging.ERROR)
+# Set to INFO for tracking training, default is WARN
 
 print("Using TensorFlow version %s" % (tf.__version__))
 
-COLUMNS = temp_list
+CATEGORICAL_COLUMNS = ['exchangeCD','ListSectorCD']
 
-df = pd.read_csv("new_stock.csv", header=None, names=COLUMNS)
-#print(df.head())
+# Columns of the input csv file
+COLUMNS = ['ticker', 'dailyReturnNoReinv', 'exchangeCD','ListSectorCD' 'totalShares', 'nonrestFloatShares', 'nonrestfloatA', 'TShEquity',
+            'AccountsPayablesTDays', 'AccountsPayablesTRate', 'AdminiExpenseRate', 'ARTDays', 'ARTRate', 'ASSI', 'BLEV',
+           'BondsPayableToAsset', 'CashRateOfSales', 'CashToCurrentLiability', 'CMRA', 'CTOP', 'CTP5', 'CurrentAssetsRatio',
+           'CurrentAssetsTRate', 'CurrentRatio', 'DAVOL10', 'DAVOL20', 'DAVOL5', 'DDNBT', 'DDNCR', 'DDNSR', 'DebtEquityRatio',
+           'DebtsAssetRatio', 'DHILO', 'DilutedEPS', 'DVRAT', 'EBITToTOR', 'EGRO', 'EMA10', 'EMA120', 'EMA20', 'EMA5', 'EMA60',
+           'EPS', 'EquityFixedAssetRatio', 'EquityToAsset', 'EquityTRate', 'ETOP', 'ETP5', 'FinancialExpenseRate', 'FinancingCashGrowRate',
+           'FixAssetRatio', 'FixedAssetsTRate', 'GrossIncomeRatio', 'HBETA', 'HSIGMA', 'IntangibleAssetRatio', 'InventoryTDays',
+           'InventoryTRate', 'InvestCashGrowRate', 'LCAP', 'LFLO', 'LongDebtToAsset', 'LongDebtToWorkingCapital', 'LongTermDebtToAsset',
+           'MA10', 'MA120', 'MA20', 'MA5', 'MA60', 'MAWVAD', 'MFI', 'MLEV', 'NetAssetGrowRate', 'NetProfitGrowRate', 'NetProfitRatio',
+           'NOCFToOperatingNI', 'NonCurrentAssetsRatio', 'NPParentCompanyGrowRate', 'NPToTOR', 'OperatingExpenseRate',
+           'OperatingProfitGrowRate', 'OperatingProfitRatio', 'OperatingProfitToTOR', 'OperatingRevenueGrowRate', 'OperCashGrowRate',
+           'OperCashInToCurrentLiability', 'PB', 'PCF', 'PE', 'PS', 'PSY', 'QuickRatio', 'REVS10', 'REVS20', 'REVS5', 'ROA', 'ROA5', 'ROE',
+           'ROE5', 'RSI', 'RSTR12', 'RSTR24', 'SalesCostRatio', 'SaleServiceCashToOR', 'SUE', 'TaxRatio', 'TOBT', 'TotalAssetGrowRate',
+           'TotalAssetsTRate', 'TotalProfitCostRatio', 'TotalProfitGrowRate', 'VOL10', 'VOL120', 'VOL20', 'VOL240', 'VOL5', 'VOL60', 'WVAD',
+           'TA2EV', 'CFO2EV', 'ACCA', 'DEGM', 'SUOI', 'EARNMOM', 'FiftyTwoWeekHigh', 'Volatility', 'Skewness', 'ILLIQUIDITY', 'BackwardADJ',
+           'MACD', 'ADTM', 'ATR14', 'ATR6', 'BIAS10', 'BIAS20', 'BIAS5', 'BIAS60', 'BollDown', 'BollUp', 'CCI10', 'CCI20', 'CCI5', 'CCI88',
+           'KDJ_K', 'KDJ_D', 'KDJ_J', 'ROC6', 'ROC20', 'SBM', 'STM', 'UpRVI', 'DownRVI', 'RVI', 'SRMI', 'ChandeSD', 'ChandeSU', 'CMO', 'DBCD',
+           'ARC', 'OBV', 'OBV6', 'OBV20', 'TVMA20', 'TVMA6', 'TVSTD20', 'TVSTD6', 'VDEA', 'VDIFF', 'VEMA10', 'VEMA12', 'VEMA26', 'VEMA5',
+           'VMACD', 'VOSC', 'VR', 'VROC12', 'VROC6', 'VSTD10', 'VSTD20', 'KlingerOscillator', 'MoneyFlow20', 'AD', 'AD20', 'AD6',
+           'CoppockCurve', 'ASI', 'ChaikinOscillator', 'ChaikinVolatility', 'EMV14', 'EMV6', 'plusDI', 'minusDI', 'ADX', 'ADXR', 'Aroon',
+           'AroonDown', 'AroonUp', 'DEA', 'DIFF', 'DDI', 'DIZ', 'DIF', 'MTM', 'MTMMA', 'PVT', 'PVT6', 'PVT12', 'TRIX5', 'TRIX10', 'UOS',
+           'MA10RegressCoeff12', 'MA10RegressCoeff6', 'PLRC6', 'PLRC12', 'SwingIndex', 'Ulcer10', 'Ulcer5', 'Hurst', 'ACD6', 'ACD20', 'EMA12',
+           'EMA26', 'APBMA', 'BBI', 'BBIC', 'TEMA10', 'TEMA5', 'MA10Close', 'AR', 'BR', 'ARBR', 'CR20', 'MassIndex', 'BearPower', 'BullPower',
+           'Elder', 'NVI', 'PVI', 'RC12', 'RC24', 'JDQS20']
 
+# Feature columns for input into the model
+FEATURE_COLUMNS = ['exchangeCD', 'ListSectorCD','totalShares', 'nonrestFloatShares', 'nonrestfloatA', 'TShEquity',
+            'AccountsPayablesTDays', 'AccountsPayablesTRate', 'AdminiExpenseRate', 'ARTDays', 'ARTRate', 'ASSI', 'BLEV',
+           'BondsPayableToAsset', 'CashRateOfSales', 'CashToCurrentLiability', 'CMRA', 'CTOP', 'CTP5', 'CurrentAssetsRatio',
+           'CurrentAssetsTRate', 'CurrentRatio', 'DAVOL10', 'DAVOL20', 'DAVOL5', 'DDNBT', 'DDNCR', 'DDNSR', 'DebtEquityRatio',
+           'DebtsAssetRatio', 'DHILO', 'DilutedEPS', 'DVRAT', 'EBITToTOR', 'EGRO', 'EMA10', 'EMA120', 'EMA20', 'EMA5', 'EMA60',
+           'EPS', 'EquityFixedAssetRatio', 'EquityToAsset', 'EquityTRate', 'ETOP', 'ETP5', 'FinancialExpenseRate', 'FinancingCashGrowRate',
+           'FixAssetRatio', 'FixedAssetsTRate', 'GrossIncomeRatio', 'HBETA', 'HSIGMA', 'IntangibleAssetRatio', 'InventoryTDays',
+           'InventoryTRate', 'InvestCashGrowRate', 'LCAP', 'LFLO', 'LongDebtToAsset', 'LongDebtToWorkingCapital', 'LongTermDebtToAsset',
+           'MA10', 'MA120', 'MA20', 'MA5', 'MA60', 'MAWVAD', 'MFI', 'MLEV', 'NetAssetGrowRate', 'NetProfitGrowRate', 'NetProfitRatio',
+           'NOCFToOperatingNI', 'NonCurrentAssetsRatio', 'NPParentCompanyGrowRate', 'NPToTOR', 'OperatingExpenseRate',
+           'OperatingProfitGrowRate', 'OperatingProfitRatio', 'OperatingProfitToTOR', 'OperatingRevenueGrowRate', 'OperCashGrowRate',
+           'OperCashInToCurrentLiability', 'PB', 'PCF', 'PE', 'PS', 'PSY', 'QuickRatio', 'REVS10', 'REVS20', 'REVS5', 'ROA', 'ROA5', 'ROE',
+           'ROE5', 'RSI', 'RSTR12', 'RSTR24', 'SalesCostRatio', 'SaleServiceCashToOR', 'SUE', 'TaxRatio', 'TOBT', 'TotalAssetGrowRate',
+           'TotalAssetsTRate', 'TotalProfitCostRatio', 'TotalProfitGrowRate', 'VOL10', 'VOL120', 'VOL20', 'VOL240', 'VOL5', 'VOL60', 'WVAD',
+           'TA2EV', 'CFO2EV', 'ACCA', 'DEGM', 'SUOI', 'EARNMOM', 'FiftyTwoWeekHigh', 'Volatility', 'Skewness', 'ILLIQUIDITY', 'BackwardADJ',
+           'MACD', 'ADTM', 'ATR14', 'ATR6', 'BIAS10', 'BIAS20', 'BIAS5', 'BIAS60', 'BollDown', 'BollUp', 'CCI10', 'CCI20', 'CCI5', 'CCI88',
+           'KDJ_K', 'KDJ_D', 'KDJ_J', 'ROC6', 'ROC20', 'SBM', 'STM', 'UpRVI', 'DownRVI', 'RVI', 'SRMI', 'ChandeSD', 'ChandeSU', 'CMO', 'DBCD',
+           'ARC', 'OBV', 'OBV6', 'OBV20', 'TVMA20', 'TVMA6', 'TVSTD20', 'TVSTD6', 'VDEA', 'VDIFF', 'VEMA10', 'VEMA12', 'VEMA26', 'VEMA5',
+           'VMACD', 'VOSC', 'VR', 'VROC12', 'VROC6', 'VSTD10', 'VSTD20', 'KlingerOscillator', 'MoneyFlow20', 'AD', 'AD20', 'AD6',
+           'CoppockCurve', 'ASI', 'ChaikinOscillator', 'ChaikinVolatility', 'EMV14', 'EMV6', 'plusDI', 'minusDI', 'ADX', 'ADXR', 'Aroon',
+           'AroonDown', 'AroonUp', 'DEA', 'DIFF', 'DDI', 'DIZ', 'DIF', 'MTM', 'MTMMA', 'PVT', 'PVT6', 'PVT12', 'TRIX5', 'TRIX10', 'UOS',
+           'MA10RegressCoeff12', 'MA10RegressCoeff6', 'PLRC6', 'PLRC12', 'SwingIndex', 'Ulcer10', 'Ulcer5', 'Hurst', 'ACD6', 'ACD20', 'EMA12',
+           'EMA26', 'APBMA', 'BBI', 'BBIC', 'TEMA10', 'TEMA5', 'MA10Close', 'AR', 'BR', 'ARBR', 'CR20', 'MassIndex', 'BearPower', 'BullPower',
+           'Elder', 'NVI', 'PVI', 'RC12', 'RC24', 'JDQS20']
+
+
+df = pd.read_csv("stock_factors.csv", header=None, names=COLUMNS)
 
 
 BATCH_SIZE = 40
+
+
+
+
+
 
 def generate_input_fn(filename, batch_size=BATCH_SIZE):
     def _input_fn():
         filename_queue = tf.train.string_input_producer([filename])
         reader = tf.TextLineReader()
         # Reads out batch_size number of lines
-        key, rows = reader.read_up_to(filename_queue,
-                                       num_records=batch_size)
+        key, value = reader.read_up_to(filename_queue, num_records=batch_size)
 
         # record_defaults should match the datatypes of each respective column.
-        record_defaults = []
-        for i in len(COLUMNS):
-            record_defaults.append([0])
+        record_defaults = [[" "], [0.0], [" "], [0]]
+        for i in range(0,237):
+            record_defaults.append([0.0])
+        #print(record_defaults)
 
-#         rows = tf.Print(rows, [tf.shape(rows)], 'rows before expanding')
-
-        # changes from shape = [Batch_size] to shape = [Batch_size, 1]
-        rows = tf.expand_dims(rows, axis=-1)
-#         rows = rows[:, np.newaxis]
-
-#         rows = tf.Print(rows, [tf.shape(rows)], 'rows after expanding')
 
         # Decode CSV data that was just read out.
         columns = tf.decode_csv(
-            rows, record_defaults=record_defaults)
+            value, record_defaults=record_defaults)
 
         # features is a dictionary that maps from column names to tensors of the data.
         # income_bracket is the last column of the data. Note that this is NOT a dict.
@@ -62,50 +101,27 @@ def generate_input_fn(filename, batch_size=BATCH_SIZE):
 
         # Save the income_bracket column as our labels
         # dict.pop() returns the popped array of income_bracket values
-        income_bracket = all_columns.pop('income_bracket')
+        dailyReturn = all_columns.pop('dailyReturnNoReinv')
 
         # remove the fnlwgt key, which is not used
-        all_columns.pop('fnlwgt', 'fnlwgt key not found')
+        all_columns.pop('ticker', 'ticker key not found')
 
         # the remaining columns are our features
         features = all_columns
 
+
+        # Sparse categorical features must be represented with an additional dimension.
+        # There is no additional work needed for the Continuous columns; they are the unaltered columns.
+        # See docs for tf.SparseTensor for more info
+        for feature_name in CATEGORICAL_COLUMNS:
+            # Requires tensorflow >= 0.12
+            features[feature_name] = tf.expand_dims(features[feature_name], -1)
+
         # Convert ">50K" to 1, and "<=50K" to 0
-        labels = tf.to_int32(tf.equal(income_bracket, " >50K"))
+        labels = tf.greater(dailyReturn, 0.0)
 
         return features, labels
 
     return _input_fn
 
 print('input function configured')
-
-
-
-# The layers module contains many utilities for creating feature columns.
-from tensorflow.contrib import layers
-
-# Sparse base columns.
-gender = layers.sparse_column_with_keys(column_name="gender",
-                                        keys=["female", "male"])
-race = layers.sparse_column_with_keys(column_name="race",
-                                      keys=["Amer-Indian-Eskimo",
-                                            "Asian-Pac-Islander",
-                                            "Black", "Other",
-                                            "White"])
-
-education = layers.sparse_column_with_hash_bucket(
-  "education", hash_bucket_size=1000)
-marital_status = layers.sparse_column_with_hash_bucket(
-  "marital_status", hash_bucket_size=100)
-relationship = layers.sparse_column_with_hash_bucket(
-  "relationship", hash_bucket_size=100)
-workclass = layers.sparse_column_with_hash_bucket(
-  "workclass", hash_bucket_size=100)
-occupation = layers.sparse_column_with_hash_bucket(
-  "occupation", hash_bucket_size=1000)
-native_country = layers.sparse_column_with_hash_bucket(
-  "native_country", hash_bucket_size=1000)
-
-print('Sparse columns configured')
-
-
